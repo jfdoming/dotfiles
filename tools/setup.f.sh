4e47b094cc2fb6767296d6a3ab26faba24fd28fa
Julian Dominguez-Schatz <julian.dominguezschatz@gmail.com>
2019-11-04 00:49:08 -0500

    Add setup script: tools/setup.f.sh

---
+++ tools/setup.f.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+
+if ! command -v git > /dev/null; then
+    echo "Could not locate the git executable. Are you sure you added it to your path?"
+    exit 1
+fi
+
+dotfiles() {
+    git --git-dir=$HOME/.dotfiles --work-tree=$HOME $@
+}
+
+if ! dotfiles checkout; then
+    # Failed!
+    echo "Failed to checkout repository! Remove any existing dotfiles and try again."
+    exit 1
+fi
+
+dotfiles config --local status.showUntrackedFiles no
+
+echo "Setup finished. You can now use your dotfiles as normal!"
+echo "You can use the \`.f\` command to update your dotfiles."
